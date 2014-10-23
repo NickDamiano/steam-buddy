ecc2fa4b7714f80fd973c2b8d2201f671862231b
Yusef Ouda <yiouda92@gmail.com>
2014-10-23 11:29:01 -0500

    Wrote tests to get a list of users games and implemented method

--- spec/steam_repo_spec.rb
+++ spec/steam_repo_spec.rb
@@ -8,10 +8,17 @@ RSpec.describe SteamRepo do
       expect(hash[:profile]).to_not be_nil
     end
     it "should return a hash when the user is not found" do
-      name = "yauihsdfauihsdfhuiasdfhu"
+      name = "yauihsdfauihsdf98237498234dhjsafdafhuiasdfhu"
       hash = SteamRepo.get_user_summary(name)
       expect(hash[:success?]).to be false
       expect(hash[:error]).to eq("The specified profile could not be found.")
+    end
+
+    it "should return a hash when user enters invalid name (invalid url)" do
+      name = "    "
+      hash = SteamRepo.get_user_summary(name)
+      expect(hash[:success?]).to be false
+      expect(hash[:error]).to eq("Invalid Steam name")
     end
 
     it "should return a hash when the user is private" do
@@ -23,5 +30,17 @@ RSpec.describe SteamRepo do
   end
 
   describe ".get_user_games" do
-  end  
+    it "should return a hash with success true and a list of games the user has" do
+      id = 76561197995163285
+      response = SteamRepo.get_user_games(id)
+      expect(response[:success?]).to be true
+      expect(response[:games]).to be_an(Array)
+      expect(response[:games].size).to be > 200
+      expect(response[:games].size).to be < 300
+    end
+  end
+
+  describe '.get_games_descriptions' do
+
+  end
 end
