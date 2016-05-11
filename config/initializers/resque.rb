92a75ebb708709064e16e821974a9a7fa8d061e7
Yusef Ouda <yiouda92@gmail.com>
2016-05-11 17:58:11 -0500

    more changes for cap

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -2,5 +2,5 @@ Dir["/app/app/jobs/*.rb"].each { |file| require file }
 rails_root = ENV['RAILS_ROOT'] || File.dirname(__FILE__) + '/../..'
 rails_env = ENV['RAILS_ENV'] || 'development'
 resque_config = YAML.load_file(rails_root + '/config/resque.yml')
-Resque.redis = resque_config[rails_env]
+Resque.redis = Redis.new(:url => resque_config[rails_env])
 Resque.logger = Logger.new(Rails.root.join('log', "#{rails_env}_resque.log"))
