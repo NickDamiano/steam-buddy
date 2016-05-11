b3f14be4d3fddab1d605d54bf731e18e2ca01955
Yusef Ouda <yiouda92@gmail.com>
2016-05-11 18:16:03 -0500

    redis fix

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -2,5 +2,6 @@ Dir["/app/app/jobs/*.rb"].each { |file| require file }
 rails_root = ENV['RAILS_ROOT'] || File.dirname(__FILE__) + '/../..'
 rails_env = ENV['RAILS_ENV'] || 'development'
 resque_config = YAML.load(ERB.new(File.new(rails_root + '/config/resque.yml.erb').read).result)
-Resque.redis = Redis.new(:url => resque_config[rails_env])
+uri = URI.parse(resque_config[rails_env])
+Resque.redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
 Resque.logger = Logger.new(Rails.root.join('log', "#{rails_env}_resque.log"))
